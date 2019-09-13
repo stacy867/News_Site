9c69f53c77d67bc23e775a9b0e5affbe88da1692
laetitia123 <laetitiauw1@gmail.com>
2019-09-13 16:40:29 +0200

    added a new file for requesting the api and url

---
+++ app/requests.py
@@ -0,0 +1,12 @@
+import urllib.request,json
+from .models import Source
+
+#Getting api key
+api_key = None
+#Getting the news base url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
